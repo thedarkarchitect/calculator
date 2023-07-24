19e0caeb0aa6303e9a0dd05d3441ff2aff6a6f64
thedarkarchitect <romanreynolds240@gmail.com>
2023-07-24 11:17:19 +0300

    buttons and functionality added

--- Calculator/Form1.Designer.vb
+++ Calculator/Form1.Designer.vb
@@ -22,10 +22,233 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        txtDisplay = New TextBox()
+        btn1 = New Button()
+        btn2 = New Button()
+        btn3 = New Button()
+        btn4 = New Button()
+        btn5 = New Button()
+        btn6 = New Button()
+        btn7 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btnClear = New Button()
+        btn0 = New Button()
+        btnAdd = New Button()
+        btnSubtract = New Button()
+        btndivide = New Button()
+        btnMultiply = New Button()
+        btnDot = New Button()
+        btnEquals = New Button()
+        SuspendLayout()
+        ' 
+        ' txtDisplay
+        ' 
+        txtDisplay.Location = New Point(12, 12)
+        txtDisplay.Multiline = True
+        txtDisplay.Name = "txtDisplay"
+        txtDisplay.Size = New Size(332, 59)
+        txtDisplay.TabIndex = 0
+        txtDisplay.Text = "0"
+        ' 
+        ' btn1
+        ' 
+        btn1.Location = New Point(21, 98)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(67, 57)
+        btn1.TabIndex = 1
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Location = New Point(106, 98)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(67, 57)
+        btn2.TabIndex = 2
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Location = New Point(190, 98)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(67, 57)
+        btn3.TabIndex = 3
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Location = New Point(21, 174)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(67, 57)
+        btn4.TabIndex = 4
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Location = New Point(106, 174)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(67, 57)
+        btn5.TabIndex = 5
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Location = New Point(190, 174)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(67, 57)
+        btn6.TabIndex = 6
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Location = New Point(21, 249)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(67, 57)
+        btn7.TabIndex = 7
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Location = New Point(106, 249)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(67, 57)
+        btn8.TabIndex = 8
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Location = New Point(190, 249)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(67, 57)
+        btn9.TabIndex = 9
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(277, 98)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(67, 57)
+        btnClear.TabIndex = 10
+        btnClear.Text = "C"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Location = New Point(21, 323)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(67, 57)
+        btn0.TabIndex = 11
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(277, 174)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(67, 57)
+        btnAdd.TabIndex = 12
+        btnAdd.Text = "+"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnSubtract
+        ' 
+        btnSubtract.Location = New Point(277, 249)
+        btnSubtract.Name = "btnSubtract"
+        btnSubtract.Size = New Size(67, 57)
+        btnSubtract.TabIndex = 13
+        btnSubtract.Text = "-"
+        btnSubtract.UseVisualStyleBackColor = True
+        ' 
+        ' btndivide
+        ' 
+        btndivide.Location = New Point(277, 323)
+        btndivide.Name = "btndivide"
+        btndivide.Size = New Size(67, 57)
+        btndivide.TabIndex = 14
+        btndivide.Text = "/"
+        btndivide.UseVisualStyleBackColor = True
+        ' 
+        ' btnMultiply
+        ' 
+        btnMultiply.Location = New Point(190, 323)
+        btnMultiply.Name = "btnMultiply"
+        btnMultiply.Size = New Size(67, 57)
+        btnMultiply.TabIndex = 15
+        btnMultiply.Text = "*"
+        btnMultiply.UseVisualStyleBackColor = True
+        ' 
+        ' btnDot
+        ' 
+        btnDot.Location = New Point(106, 323)
+        btnDot.Name = "btnDot"
+        btnDot.Size = New Size(67, 57)
+        btnDot.TabIndex = 16
+        btnDot.Text = "."
+        btnDot.UseVisualStyleBackColor = True
+        ' 
+        ' btnEquals
+        ' 
+        btnEquals.Location = New Point(12, 403)
+        btnEquals.Name = "btnEquals"
+        btnEquals.Size = New Size(332, 57)
+        btnEquals.TabIndex = 17
+        btnEquals.Text = "="
+        btnEquals.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(360, 491)
+        Controls.Add(btnEquals)
+        Controls.Add(btnDot)
+        Controls.Add(btnMultiply)
+        Controls.Add(btndivide)
+        Controls.Add(btnSubtract)
+        Controls.Add(btnAdd)
+        Controls.Add(btn0)
+        Controls.Add(btnClear)
+        Controls.Add(btn9)
+        Controls.Add(btn8)
+        Controls.Add(btn7)
+        Controls.Add(btn6)
+        Controls.Add(btn5)
+        Controls.Add(btn4)
+        Controls.Add(btn3)
+        Controls.Add(btn2)
+        Controls.Add(btn1)
+        Controls.Add(txtDisplay)
+        Name = "Form1"
+        Text = "CalculatorApp"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents txtDisplay As TextBox
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btndivide As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnDot As Button
+    Friend WithEvents btnEquals As Button
 End Class
--- Calculator/Form1.vb
+++ Calculator/Form1.vb
@@ -1,3 +1,149 @@
 ﻿Public Class Form1
+    Dim FirstNum As Decimal
+    Dim SecondtNum As Decimal
+    Dim operators As Integer
+    Dim operator_selector As Boolean = False
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        If txtDisplay.Text <> "0" Then
+            txtDisplay.Text += "1"
+        Else
+            txtDisplay.Text = "1"
+        End If
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        If txtDisplay.Text <> "0" Then
+            txtDisplay.Text += "2"
+        Else
+            txtDisplay.Text = "2"
+        End If
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        If txtDisplay.Text <> "0" Then
+            txtDisplay.Text += "3"
+        Else
+            txtDisplay.Text = "3"
+        End If
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        If txtDisplay.Text <> "0" Then
+            txtDisplay.Text += "4"
+        Else
+            txtDisplay.Text = "4"
+        End If
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        If txtDisplay.Text <> "0" Then
+            txtDisplay.Text += "5"
+        Else
+            txtDisplay.Text = "5"
+        End If
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        If txtDisplay.Text <> "0" Then
+            txtDisplay.Text += "6"
+        Else
+            txtDisplay.Text = "6"
+        End If
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        If txtDisplay.Text <> "0" Then
+            txtDisplay.Text += "7"
+        Else
+            txtDisplay.Text = "7"
+        End If
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        If txtDisplay.Text <> "0" Then
+            txtDisplay.Text += "8"
+        Else
+            txtDisplay.Text = "8"
+        End If
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        If txtDisplay.Text <> "0" Then
+            txtDisplay.Text += "9"
+        Else
+            txtDisplay.Text = "9"
+        End If
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        If txtDisplay.Text <> "0" Then
+            txtDisplay.Text += "0"
+        End If
+    End Sub
+
+    Private Sub btnDot_Click(sender As Object, e As EventArgs) Handles btnDot.Click
+        If Not (txtDisplay.Text.Contains(".")) Then
+            txtDisplay.Text += "."
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtDisplay.Text = "0"
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        FirstNum = txtDisplay.Text
+        txtDisplay.Text = "0"
+        operator_selector = True
+        operators = 1 '+ addition
+    End Sub
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        FirstNum = txtDisplay.Text
+        txtDisplay.Text = "0"
+        operator_selector = True
+        operators = 2 '- subtract
+    End Sub
+
+    Private Sub btndivide_Click(sender As Object, e As EventArgs) Handles btndivide.Click
+        FirstNum = txtDisplay.Text
+        txtDisplay.Text = "0"
+        operator_selector = True
+        operators = 3 '/ divide
+    End Sub
+
+    Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        FirstNum = txtDisplay.Text
+        txtDisplay.Text = "0"
+        operator_selector = True
+        operators = 4 '* multiply
+    End Sub
+
+    Private Sub btnEquals_Click(sender As Object, e As EventArgs) Handles btnEquals.Click
+        If operator_selector = True Then
+            SecondtNum = txtDisplay.Text
+            If operators = 1 Then
+                txtDisplay.Text = FirstNum + SecondtNum
+            ElseIf operators = 2 Then
+                txtDisplay.Text = FirstNum - SecondtNum
+            ElseIf operators = 3 Then
+                txtDisplay.Text = FirstNum / SecondtNum
+            Else
+                If SecondtNum = 0 Then
+                    txtDisplay.Text = "Error!"
+                Else
+                    txtDisplay.Text = FirstNum * SecondtNum
+                End If
+            End If
+            operator_selector = False
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
 
 End Class
